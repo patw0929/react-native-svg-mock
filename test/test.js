df1ae7786457f6bb07324308be9c4ec7981304ee
Patrick <patw.hi@gmail.com>
2017-05-09 17:51:23 +0800

    test script

--- test/test.js
+++ test/test.js
@@ -1,20 +1,53 @@
-require("react-native-mock/mock");
+require('react-native-mock/mock');
 
-var React = require("react-native");
-var ReactNativeSvg = require("../");
+import
+  ReactNativeSvg,
+  {
+    Svg,
+    Circle,
+    Ellipse,
+    G,
+    Text,
+    Path,
+    Polygon,
+    Polyline,
+    Line,
+    Rect,
+    Use,
+    Image,
+    Symbol,
+    Defs,
+    LinearGradient,
+    RadialGradient,
+    Stop,
+    ClipPath,
+  }
+from '../src/index';
+import { expect } from 'chai';
 
-var expect = require("chai").expect;
-var render = require("enzyme").render;
-
-describe("mock", function () {
-  it("requires", function () {
+describe('mock', function () {
+  it('requires', function () {
     expect(Object.keys(ReactNativeSvg)).to.have.length.above(1);
   });
 
-  it("renders", function () {
-    for (var prop in ReactNativeSvg) {
-      var comp = ReactNativeSvg[prop];
-      render(React.createElement(comp));
-    }
+  it('returns renderable types when expected', () => {
+    expect(Svg).to.be.a('function');
+    expect(Circle).to.be.a('function');
+    expect(Ellipse).to.be.a('function');
+    expect(G).to.be.a('function');
+    expect(Text).to.be.a('function');
+    expect(Path).to.be.a('function');
+    expect(Polygon).to.be.a('function');
+    expect(Polyline).to.be.a('function');
+    expect(Line).to.be.a('function');
+    expect(Rect).to.be.a('function');
+    expect(Use).to.be.a('function');
+    expect(Image).to.be.a('function');
+    expect(Symbol).to.be.a('function');
+    expect(Defs).to.be.a('function');
+    expect(LinearGradient).to.be.a('function');
+    expect(RadialGradient).to.be.a('function');
+    expect(Stop).to.be.a('function');
+    expect(ClipPath).to.be.a('function');
   });
 });
