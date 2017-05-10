e68b626eadae8378b0347e85a0c75969a1547561
Patrick <patw.hi@gmail.com>
2017-05-10 09:32:58 +0800

    Rebuild

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.ClipPath=exports.Stop=exports.RadialGradient=exports.LinearGradient=exports.Defs=exports.Symbol=exports.Image=exports.Use=exports.Rect=exports.Line=exports.Polyline=exports.Polygon=exports.Path=exports.Text=exports.G=exports.Ellipse=exports.Circle=exports.Svg=undefined;var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require('react');var _react2=_interopRequireDefault(_react);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{'default':obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}
+Object.defineProperty(exports,"__esModule",{value:true});exports.ClipPath=exports.Stop=exports.RadialGradient=exports.LinearGradient=exports.Defs=exports.Symbol=exports.Image=exports.Use=exports.Rect=exports.Line=exports.Polyline=exports.Polygon=exports.Path=exports.Text=exports.G=exports.Ellipse=exports.Circle=exports.Svg=undefined;var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require('react');var _react2=_interopRequireDefault(_react);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}
 
 // Create a vanilla SVG component
-var createComponent=function(){function createComponent(name){var _class,_temp;return _temp=_class=function(_Component){_inherits(_class,_Component);function _class(){_classCallCheck(this,_class);return _possibleConstructorReturn(this,(_class.__proto__||Object.getPrototypeOf(_class)).apply(this,arguments));}_createClass(_class,[{key:'render',value:function(){function render()
+var createComponent=function createComponent(name){var _class,_temp;return _temp=_class=function(_Component){_inherits(_class,_Component);function _class(){_classCallCheck(this,_class);return _possibleConstructorReturn(this,(_class.__proto__||Object.getPrototypeOf(_class)).apply(this,arguments));}_createClass(_class,[{key:'render',value:function render()
 
 
 
@@ -11,8 +11,8 @@ var createComponent=function(){function createComponent(name){var _class,_temp;r
 {
 // const type = name[0].toLowerCase() + name.substr(1);
 
-return _react2['default'].createElement('react-native-svg-mock',null,this.props.children);
-}return render;}()}]);return _class;}(_react.Component),_class.displayName=name,_class.propTypes={children:_react.PropTypes.node},_temp;}return createComponent;}();
+return _react2.default.createElement('react-native-svg-mock',null,this.props.children);
+}}]);return _class;}(_react.Component),_class.displayName=name,_class.propTypes={children:_react.PropTypes.node},_temp;};
 
 
 // Mock all react-native-svg exports
@@ -29,7 +29,7 @@ var Line=createComponent('Line');
 var Rect=createComponent('Rect');
 var Use=createComponent('Use');
 var Image=createComponent('Image');
-var _Symbol=createComponent('Symbol');
+var Symbol=createComponent('Symbol');
 var Defs=createComponent('Defs');
 var LinearGradient=createComponent('LinearGradient');
 var RadialGradient=createComponent('RadialGradient');
@@ -49,12 +49,12 @@ Line=Line;exports.
 Rect=Rect;exports.
 Use=Use;exports.
 Image=Image;exports.
-Symbol=_Symbol;exports.
+Symbol=Symbol;exports.
 Defs=Defs;exports.
 LinearGradient=LinearGradient;exports.
 RadialGradient=RadialGradient;exports.
 Stop=Stop;exports.
-ClipPath=ClipPath;exports['default']=
+ClipPath=ClipPath;exports.default=
 
 
 Svg;
